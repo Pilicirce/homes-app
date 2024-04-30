1af50b0afce4ccb8ca3bfe8db5a550e8b8788acc
Pilar Solis <pilar.sm02@gmail.com>
2024-04-30 16:48:52 +0200

    modify styles

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
+ platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err)); 
+ 
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/* bootstrapApplication(AppComponent,
+    {providers: [provideProtractorTestingSupport()]})
+  .catch(err => console.error(err)); */
