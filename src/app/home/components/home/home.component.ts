9507a92db44caefe2c4f4fc71b018f35fde78ec8
Pilar Solis <pilar.sm02@gmail.com>
2024-04-30 16:25:00 +0200

    add interface housinglocation.ts

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../../interfaces/housinglocation';
 
 @Component({
   selector: 'app-home',
   template: `
- <section>
-  <form>
-    <input type="text" placeholder="Filter by city">
-    <button class="primary" type="button"> Search </button>
-  </form>
- </section>
+<section>
+<form>
+<input type="text" placeholder="Filter by city">
+<button class="primary" type="button">Search</button>
+</form>
+</section>
+<section class="results">
+<app-housing-location></app-housing-location>
+</section>
 `,
-
-
 
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
 
+
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+};
 }
+
+
