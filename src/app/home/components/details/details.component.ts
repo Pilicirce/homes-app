4e9835bf0f50c79b76ed29a30064e6294946bd08
Pilar Solis <pilar.sm02@gmail.com>
2024-05-14 07:41:46 +0200

    obtain parameters of route

--- src/app/home/components/details/details.component.ts
+++ src/app/home/components/details/details.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../../services/housing.service';
+import { HousingLocation } from '../../interfaces/housinglocation';
 
 @Component({
   selector: 'app-details',
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
+  template:  `<p>details works! {{ housingLocationId }}</p>`,
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+  this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
