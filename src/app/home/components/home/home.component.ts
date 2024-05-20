f26fbbd61e67be594c6d2c1b4044217391540880
Pilar Solis <pilar.sm02@gmail.com>
2024-05-20 07:39:05 +0200

    add filteredResults to home.component

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -9,12 +9,12 @@ import { HousingService } from '../../services/housing.service';
   template: `
 <section>
 <form>
-<input type="text" placeholder="Filter by city">
-<button class="primary" type="button">Search</button>
+<input type="text" placeholder="Filter by city" #filter>
+<button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
 </form>
 </section>
 <section class="results">
-<app-housing-location *ngFor="let housingLocation of housingLocationList" 
+<app-housing-location *ngFor="let housingLocation of filteredLocationList" 
       [housingLocation]="housingLocation"></app-housing-location>
 </section>
 `,
@@ -30,6 +30,16 @@ housingLocationList: HousingLocation[] = [];
 filteredLocationList: HousingLocation[] = [];
 
 housingService: HousingService = inject(HousingService);
+
+filterResults(text: string) {
+  if(!text) {
+  this.filteredLocationList = this.housingLocationList;
+  return;
+  }
+  this.filteredLocationList = this.housingLocationList.filter(
+    housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+    }
 
 constructor() {
   this.housingLocationList = this.housingService.getAllHousingLocations();
