43f56efa074e175e85b9f9127bebf87d448f5362
Pilar Solis <pilar.sm02@gmail.com>
2024-06-18 16:30:06 +0200

    fix search filter in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //standalone: false,
- // imports: [],
- templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
-// imports: [
-//  HomeComponent,
-//  RouterModule,
-//  ],
-  
-
-  //en la practica que hacemos, ellos hacen la aplicación sin módulos. Yo como tengo una versión de node antigua y ademas queiro practicar, la hago con módulos
- // selector: 'app-root',
- // templateUrl: './app.component.html',
- // styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'homes-app'; //esta linea no se si debo dejarla o eliminarla despues de haber metido el enrutamiento
+  title = 'homes-app'; 
 }
--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -13,25 +13,37 @@ import { HousingService } from '../../services/housing.service';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
  
-housingLocationList: HousingLocation[] = [];
-filteredLocationList: HousingLocation[] = [];
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
 
-housingService: HousingService = inject(HousingService);
+  housingService: HousingService = inject(HousingService);
 
-constructor() {
-  this.housingLocationList = this.housingService.getAllHousingLocations();
-  this.filteredLocationList = this.housingLocationList;
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    console.log('Initial housing locations:', this.housingLocationList);
+    this.filteredLocationList = this.housingLocationList;
   }
 
-  
   filterResults(text: string) {
+    console.log('Filtering results for:', text);
     if (!text) {
       this.filteredLocationList = this.housingLocationList;
+      console.log('No filter text, showing all locations');
       return;
     }
     this.filteredLocationList = this.housingLocationList.filter(
-      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
-
+      housingLocation => housingLocation.city.toLowerCase().includes(text.toLowerCase())
     );
+    console.log('Filtered locations:', this.filteredLocationList);
+  }
+
+  logClick() {
+    console.log('Button clicked');
+  }
+
+  onSubmit(event: Event, text: string) {
+    event.preventDefault();  // Evitar el comportamiento predeterminado de envío del formulario
+    console.log('Form submitted with text:', text);
+    this.filterResults(text);
   }
 }
