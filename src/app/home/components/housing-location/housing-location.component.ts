60d0775d5efdede9c6480b861968a9f38a8d9637
Pilar Solis <pilar.sm02@gmail.com>
2024-06-18 13:15:08 +0200

    separete housing-location.component.html from .ts

--- src/app/home/components/housing-location/housing-location.component.ts
+++ src/app/home/components/housing-location/housing-location.component.ts
@@ -3,14 +3,7 @@ import { HousingLocation } from '../../interfaces/housinglocation';
 
 @Component({
   selector: 'app-housing-location',
-  template: `
-  <section class="listing">
-  <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-  <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-  <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
-  </section>
-  `,
+  templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
