b133bd989c57e14b8ba06fc3c0812ca6fdeebdcc
Pilar Solis <pilar.sm02@gmail.com>
2026-05-04 08:42:53 +0200

    New component: create-housing.component

--- projects/front/src/app/home/components/create-housing/create-housing.component.ts
+++ projects/front/src/app/home/components/create-housing/create-housing.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HousingService } from '../../services/housing.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-housing',
   templateUrl: './create-housing.component.html',
   styleUrls: ['./create-housing.component.css']
 })
-export class CreateHousingComponent {
 
+export class CreateHousingComponent implements OnInit {
+
+  housingForm!: FormGroup;
+
+  constructor(
+    private housingService: HousingService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.housingForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      photo: new FormControl(''),
+      availableUnits: new FormControl(0, Validators.required),
+      bedrooms: new FormControl(0, Validators.required),
+      wifi: new FormControl(false),
+      laundry: new FormControl(false),
+      parking: new FormControl(false)
+    });
+  }
+
+  onSubmit(): void {
+    if (this.housingForm.valid) {
+      this.housingService.createHousingLocation(this.housingForm.value)
+        .subscribe({
+          next: () => {
+            console.log('Housing created');
+            this.router.navigate(['/']);
+          },
+          error: err => console.error('Error creating housing', err)
+        });
+    }
+  }
 }
