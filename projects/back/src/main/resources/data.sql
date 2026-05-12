fcca65758a27539cbeea06d9c10bf49ed1d85371
Pilar Solis <pilar.sm02@gmail.com>
2026-05-12 09:27:05 +0200

    Add images to initial housing data and improve UI with fallback image handling

--- projects/back/src/main/resources/data.sql
+++ projects/back/src/main/resources/data.sql
@@ -1,9 +1,40 @@
 INSERT INTO housing
 (name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
 VALUES
-('Sunny Apartments', 'Murcia', 'Spain', 'house1.jpg', 3, true, true, 2, true);
+('Sunny Apartments', 'Murcia', 'Spain', 'http://localhost:8081/images/sunnyApartments.jpg', 3, true, true, 2, true);
 
 INSERT INTO housing
 (name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
 VALUES
-('Central Flat', 'Murcia', 'Spain', 'flat1.jpg', 1, true, false, 1, false);
+('Transitional Housing', 'Oslo', 'Norway', 'http://localhost:8081/images/transitionalHousing.jpg', 4, true, true, 1, false);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('Casa Andaluza', 'Granada', 'Spain', 'http://localhost:8081/images/casaAndaluza.jpg', 1, true, false, 1, false);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('Home Steady House', 'Nueva Jersey', 'EEUU', 'http://localhost:8081/images/homesteadyHouse.jpg', 1, true, false, 3, true);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('Orange House', 'Málaga', 'Spain', 'http://localhost:8081/images/orangeHouse.jpg', 4, false, false, 2, false);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('White House', 'Valencia', 'Spain', 'http://localhost:8081/images/whiteHouse.jpg', 1, true, true, 4, true);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('Hopeful Apartments', 'Oslo', 'Norway', 'http://localhost:8081/images/hopefulApartmentGroup.jpg', 5, true, false, 2, true);
+
+INSERT INTO housing
+(name, city, country, photo, available_units, wifi, laundry, bedrooms, parking)
+VALUES
+('Happy Homes', 'Murcia', 'Spain', 'http://localhost:8081/images/happyHomesGroup.jpg', 4, true, false, 3, true);
+
